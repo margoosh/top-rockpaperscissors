8f086e395d41713689e3a011ed0e81cefbb26bfd
margoosh <192158750+margoosh@users.noreply.github.com>
2025-02-21 13:48:49 -0800

    created and tested getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
 console.log('We\'ve made contact');
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    switch (computerChoice) {
+        case 0:
+            return 'Rock';
+        case 1: 
+            return 'Paper';
+        case 2: 
+            return 'Scissors';
+    }  
+};
+
+console.log(`Welcome to Rock Paper Scissors!`);
+
+console.log(`Computer Chose: ${getComputerChoice()}`);
